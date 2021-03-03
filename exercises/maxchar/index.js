9f067e377b119cbd07f546d5bd701a08b6e0c1b8
Your Name <you@example.com>
2021-03-03 23:10:17 +0530

    written a program that returns the most commonly used character in the string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+
+  let max = 0;
+  let maxChar = "";
+
+  for (const char of str) {
+    // if character is there already then add one to it's value
+    if (chars[char]) {
+      chars[char]++;
+    } else {
+      // otherwise assign that character's value 1
+      chars[char] = 1;
+    }
+  }
+
+  //looping through the chars which is the object of string with no of repeated words
+  for (const char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
